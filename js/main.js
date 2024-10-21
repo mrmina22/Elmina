e163a78a2a5420ab1bc4db5047041899a4e6a0b4
mrmina22 <mrmina22447948@gmail.com>
2024-10-21 16:02:01 +0300

    fix our skills js problem

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 let ourSkills = document.getElementById("our-skills");
 let stats = document.getElementById("stats");
 let functionStarted = false;
+function StatsInc(el) {
+    functionStarted = true;
+    el.forEach((el) => {
+      let goal = el.dataset.goal;
+      let count = setInterval(() => {
+        el.textContent++;
+        if (el.textContent == goal) {
+          clearInterval(count);
+        }
+      }, 20);
+    });
+  }
+  
 window.onscroll = function () {
   // window.scrollY represents the number of pixels the page has been scrolled vertically
   // ourSkills.offsetTop gives the distance from the top of the page to the top of the #our-skills element
@@ -15,22 +28,6 @@ window.onscroll = function () {
         el.style.width = el.dataset.goal;
       });
   }
-};
-function StatsInc(el) {
-  functionStarted = true;
-  el.forEach((el) => {
-    let goal = el.dataset.goal;
-    let count = setInterval(() => {
-      el.textContent++;
-      console.log(el.textContent);
-      console.log(goal);
-      if (el.textContent == goal) {
-        clearInterval(count);
-      }
-    }, 20);
-  });
-}
-window.onscroll = function () {
   if (window.scrollY >= stats.offsetTop - 300) {
     if (!functionStarted) {
       StatsInc(document.querySelectorAll(".stats .container .box .number"));
